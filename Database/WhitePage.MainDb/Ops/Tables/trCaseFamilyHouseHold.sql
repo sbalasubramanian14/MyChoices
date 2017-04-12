54f8abc6ec04a6f2ba3a431f786f3306956913e4
rnarimanda <reddy.arimanda@quantium.com.au>
2017-04-12 16:07:32 +0530

    Case Family & HouseHold

---
+++ Database/WhitePage.MainDb/Ops/Tables/trCaseFamilyHouseHold.sql
@@ -0,0 +1,24 @@
+﻿CREATE TABLE [dbo].[trCaseFamilyHouseHold]
+(
+	[CaseFamilyHouseHoldId] INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+	[CaseId] INT NOT NULL,
+	ChildrenDeceasedLookupId INT NULL,
+	HouseHoldIncomeLookupId INT NULL,
+	SoughtHelpYesNoLookupId INT NULL,
+	SoughtHelpDesc varchar(200) NULL,
+	SoughtHelpOutPut varchar(200) NULL,
+	PeacemakerAssistanceLookupId varchar(200) NULL,
+	PeacemakerAssistanceDesc varchar(200),
+	PeacemakerFollowupYesNoLookupId INT NULL,
+	ClientSignedRegistrationFormYesNoLookupId INT NULL,
+	ClientEmailId varchar(200) NULL,
+	ReligionLookupId INT NULL,
+	LevelOfEducationLookupId INT NULL,
+	VocationalSkillsLookupId INT NULL,
+	OccupationLookupId INT NULL,
+	OccupationDesc varchar(200) NULL,
+	ClientIncomeLookupId INT NULL,
+	HouseHoldMembersLivingLookupId INT NULL,
+	YearOfMarriage smallint NULL,
+	ClientAgeAtFirstChild tinyint NULL
+)
