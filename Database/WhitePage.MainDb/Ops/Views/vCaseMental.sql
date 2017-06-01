1482f134aea0fe5577ebc713186338dff31d4033
samyuktha31 <samyuukthakodali@gmail.com>
2017-06-01 18:54:15 +0530

    fix: Changed view for mental assessment

--- Database/WhitePage.MainDb/Ops/Views/vCaseMental.sql
+++ Database/WhitePage.MainDb/Ops/Views/vCaseMental.sql
@@ -44,22 +44,23 @@ A54.Value as MentalInformation,
 A55.Value as MentalAbstraction
 
 FROM [Ops].[trCaseMental] A
-LEFT JOIN Core.dmnLookupDetail A30 ON A30.LookupDetailId = A.MentalDressLookupId 
-LEFT JOIN Core.dmnLookupDetail A31 ON A31.LookupDetailId = A.MentalHygieneLookupId 
-LEFT JOIN Core.dmnLookupDetail A32 ON A32.LookupDetailId = A.MentalBodyTypeLookupId 
-LEFT JOIN Core.dmnLookupDetail A33 ON A33.LookupDetailId = A.MentalExpressionLookupId 
-LEFT JOIN Core.dmnLookupDetail A34 ON A34.LookupDetailId = A.MentalMotorActivityLookupId 
-LEFT JOIN Core.dmnLookupDetail A35 ON A35.LookupDetailId = A.MentalVocabularyLookupId 
-LEFT JOIN Core.dmnLookupDetail A36 ON A36.LookupDetailId = A.MentalImpulseControlLookupId 
-LEFT JOIN Core.dmnLookupDetail A37 ON A37.LookupDetailId = A.MentalSpeechLookupId 
-LEFT JOIN Core.dmnLookupDetail A38 ON A38.LookupDetailId = A.MentalBehaviourLookupId 
-LEFT JOIN Core.dmnLookupDetail A39 ON A39.LookupDetailId = A.MentalContentLookupId 
-LEFT JOIN Core.dmnLookupDetail A40 ON A40.LookupDetailId = A.MentalFlowOfThoughtLookupId 
-LEFT JOIN Core.dmnLookupDetail A41 ON A41.LookupDetailId = A.MentalOrientationLookupId 
-LEFT JOIN Core.dmnLookupDetail A42 ON A42.LookupDetailId = A.MentalEstimatedIntellectLookupId 
-LEFT JOIN Core.dmnLookupDetail A43 ON A43.LookupDetailId = A.MentalAttentionLookupId 
-LEFT JOIN Core.dmnLookupDetail A51 ON A51.LookupDetailId = A.MentalInsightLookupId 
-LEFT JOIN Core.dmnLookupDetail A52 ON A52.LookupDetailId = A.MentalJudgementLookupId 
-LEFT JOIN Core.dmnLookupDetail A53 ON A53.LookupDetailId = A.MentalMemoryLookupId 
-LEFT JOIN Core.dmnLookupDetail A54 ON A54.LookupDetailId = A.MentalInformationLookupId 
-LEFT JOIN Core.dmnLookupDetail A55 ON A55.LookupDetailId = A.MentalAbstractionLookupId 
+LEFT JOIN Core.dmnLookupDetail A30 ON A30.LookupDetailId = REPLACE(A.MentalDressLookupId, ';', ',' )
+LEFT JOIN Core.dmnLookupDetail A31 ON A31.LookupDetailId = REPLACE(A.MentalHygieneLookupId, ';', ',' )
+LEFT JOIN Core.dmnLookupDetail A32 ON A32.LookupDetailId = REPLACE(A.MentalBodyTypeLookupId, ';', ',' )
+LEFT JOIN Core.dmnLookupDetail A33 ON A33.LookupDetailId = REPLACE(A.MentalExpressionLookupId, ';', ',' )
+LEFT JOIN Core.dmnLookupDetail A34 ON A34.LookupDetailId = REPLACE(A.MentalMotorActivityLookupId, ';', ',' )
+LEFT JOIN Core.dmnLookupDetail A35 ON A35.LookupDetailId = REPLACE(A.MentalVocabularyLookupId, ';', ',' )
+LEFT JOIN Core.dmnLookupDetail A36 ON A36.LookupDetailId = REPLACE(A.MentalImpulseControlLookupId, ';', ',' )
+LEFT JOIN Core.dmnLookupDetail A37 ON A37.LookupDetailId = REPLACE(A.MentalSpeechLookupId, ';', ',' ) 
+LEFT JOIN Core.dmnLookupDetail A38 ON A38.LookupDetailId = REPLACE(A.MentalBehaviourLookupId, ';', ',' )
+LEFT JOIN Core.dmnLookupDetail A39 ON A39.LookupDetailId = REPLACE(A.MentalContentLookupId, ';', ',' )
+LEFT JOIN Core.dmnLookupDetail A40 ON A40.LookupDetailId = REPLACE(A.MentalFlowOfThoughtLookupId, ';', ',' )
+LEFT JOIN Core.dmnLookupDetail A41 ON A41.LookupDetailId = REPLACE(A.MentalOrientationLookupId, ';', ',' )
+LEFT JOIN Core.dmnLookupDetail A42 ON A42.LookupDetailId = REPLACE(A.MentalEstimatedIntellectLookupId, ';', ',' )
+LEFT JOIN Core.dmnLookupDetail A43 ON A43.LookupDetailId = REPLACE(A.MentalAttentionLookupId, ';', ',' )
+LEFT JOIN Core.dmnLookupDetail A51 ON A51.LookupDetailId = REPLACE(A.MentalInsightLookupId, ';', ',' )
+LEFT JOIN Core.dmnLookupDetail A52 ON A52.LookupDetailId = REPLACE(A.MentalJudgementLookupId, ';', ',' )
+LEFT JOIN Core.dmnLookupDetail A53 ON A53.LookupDetailId = REPLACE(A.MentalMemoryLookupId, ';', ',' )
+LEFT JOIN Core.dmnLookupDetail A54 ON A54.LookupDetailId = REPLACE(A.MentalInformationLookupId, ';', ',' )
+LEFT JOIN Core.dmnLookupDetail A55 ON A55.LookupDetailId = REPLACE(A.MentalAbstractionLookupId, ';', ',' )
+
