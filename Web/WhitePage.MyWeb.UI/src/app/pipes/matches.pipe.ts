2cbb3ab7fa98677e93e16553f748d4ac05967a5e
sudheer517 <sudheer.puthineedi@gmail.com>
2017-06-09 11:44:54 +0530

    Fix: Added comma b/w values of multi select for Case View

--- Web/WhitePage.MyWeb.UI/src/app/pipes/matches.pipe.ts
+++ Web/WhitePage.MyWeb.UI/src/app/pipes/matches.pipe.ts
@@ -7,10 +7,9 @@ export class MatchesPipe implements PipeTransform {
     transform(items: Array<any>, values: string, type: string): string {
         var elementsArray = [values];
         var isMultiSelect = type === "multiselect";
-        var returnData = "";
 
         if (values == null){
-            return returnData;
+            return "";
         }
         
         if (values.toString().indexOf(";") > -1) {
@@ -23,8 +22,8 @@ export class MatchesPipe implements PipeTransform {
 
         var lists = items.filter(item => elementsArray.includes(isMultiSelect ? item.id.toString() : item.value.toString()));
 
-        lists.forEach(list => returnData += isMultiSelect ? list.name + "  " : list.label + "  ")
+        lists.forEach(list => elementsArray[lists.indexOf(list)] = isMultiSelect ? list.name : list.label)
 
-        return returnData;
+        return elementsArray.join(", ");
     }
 }
