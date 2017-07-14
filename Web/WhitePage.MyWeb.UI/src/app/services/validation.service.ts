5b86291ce598d0f312aa9ef7ba38dfc60069fe8d
Jayaram Sudheer P <sudheer.puthineedi@gmail.com>
2017-07-14 11:50:45 +0530

    chore: added new service for validations

---
+++ Web/WhitePage.MyWeb.UI/src/app/services/validation.service.ts
@@ -0,0 +1,57 @@
+﻿import { Injectable } from '@angular/core';
+import { AbstractControl } from '@angular/forms';
+
+@Injectable()
+export class ValidationService {
+    public validateNumber(input: AbstractControl): { valid: boolean } | null {
+
+        if (input.value == null || input.value == "") {
+
+            return null
+        }
+        return input.value > 0 && input.value < 100 ? null : { valid: false };
+    };
+
+    public emailValidator(input: AbstractControl) {
+
+        var emailRegExp = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+        if (input.value == null) {
+            return null;
+        }
+
+        if (input.value != "" && (input.value.length <= 5 || !emailRegExp.test(input.value))) {
+            return { 'Please provide a valid email': true };
+        }
+
+        return null;
+    }
+
+    public numericValidator(input: AbstractControl) {
+
+        var numericPattern = /^[0-9]*$/;
+
+        if (input.value == null || input.value == "") {
+            return null;
+        }
+        if (!numericPattern.test(input.value)) {
+            return { 'Please provide a valid PIN': true };
+        }
+    }
+
+    public mobileValidator(input: AbstractControl) {
+
+        var mobilePattern = /^(\+91[\-\s]?)?[0]?(91)?[789]\d{9}$/;
+
+        if (input.value == null || input.value == "") {
+            return null;
+        }
+
+        if (!mobilePattern.test(input.value)) {
+            return { 'Please provide a valid phone': true };
+        }
+
+        return null;
+    }
+
+}
