976bae75caa6716fab8ac0d2f14ef11cf0f0ae95
jkkr1996 <jkkr.1996@gmail.com>
2017-08-20 17:34:45 +0530

    Fix: Hiding the signin button while loading after click.

--- Web/WhitePage.MyWeb.UI/src/app/pages/pages.signin.ts
+++ Web/WhitePage.MyWeb.UI/src/app/pages/pages.signin.ts
@@ -18,6 +18,7 @@ export class PagesSignInComponent implements OnInit {
     returnUrl: string;
     errorMsg: string;
     public enableSpinner: boolean = true;
+    public buttonFlag: boolean = true;
 
     constructor(private fb: FormBuilder, private route: ActivatedRoute, private router: Router,
         private authenticationService: AuthenticationService,
@@ -34,6 +35,7 @@ export class PagesSignInComponent implements OnInit {
     public user: any;
     signIn(provider: string) {
         this.enableSpinner = true;
+        this.buttonFlag = false;
         this._auth.login(provider).subscribe(data => {
             this.zone.run(() => {
                 this.user = data;
@@ -47,6 +49,7 @@ export class PagesSignInComponent implements OnInit {
                     error => {
                         this.enableSpinner = false;
                         console.log(error);
+                        this.buttonFlag = true;
                         this.errorMsg = "Your identity not authorized; please contact your administrator.";
                     });
             });
