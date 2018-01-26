8bb8ab8f060bd10eb859b30ea01150402d58f24d
Karthik Kumar <jkkr.1996@gmail.com>
2018-01-26 21:22:49 +0530

    Updated the SerialNumberTrackerRA table

--- Database/WhitePage.MainDb/Core/Tables/trSerialNumberTrackerRA.sql
+++ Database/WhitePage.MainDb/Core/Tables/trSerialNumberTrackerRA.sql
@@ -2,7 +2,7 @@
 (
 	[SerialNumberTrackerId] [int] NOT NULL PRIMARY KEY,
 	[FormType] [varchar](5) NOT NULL,
-	[IpCode] [int] NOT NULL,
+	[IpCode] [VARCHAR](10) NOT NULL,
 	[SerialValue] [int] NOT NULL,
 	[GeneratedDate] [datetime] NOT NULL
 )
