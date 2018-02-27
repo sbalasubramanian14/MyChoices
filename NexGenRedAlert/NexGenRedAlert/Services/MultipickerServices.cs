7e868429232282d3f0b7b5251c62c1b9abfa5a0f
karthikkumar1996 <jkkr.1996@gmail.com>
2018-02-27 23:45:47 +0530

    Multipickers and MultipickerServices

---
+++ NexGenRedAlert/NexGenRedAlert/Services/MultipickerServices.cs
@@ -0,0 +1,44 @@
+﻿
+using NexGenRedAlert.Models;
+using System.Collections.Generic;
+
+namespace NexGenRedAlert.Services
+{
+    public class MultipickerServices
+    {
+        private List<MultiselectItem> _majorSourceOfIncomeItems;
+        private List<MultiselectItem> _localIssuesItems;
+
+        public List<MultiselectItem> MajorSourceOfIncomeItems
+        {
+            get
+            {
+                _majorSourceOfIncomeItems = new List<MultiselectItem>
+                {
+                    new MultiselectItem { Name = "Agriculture" },
+                    new MultiselectItem { Name = "Industrial" },
+                    new MultiselectItem { Name = "Casual Labour" },
+                    new MultiselectItem { Name = "Self-Employed" },
+                    new MultiselectItem { Name = "Other" }
+                };
+                return _majorSourceOfIncomeItems;
+            }
+        }
+
+        public List<MultiselectItem> LocalIssuesItems
+        {
+            get
+            {
+                _localIssuesItems = new List<MultiselectItem>
+                {
+                    new MultiselectItem { Name = "Prone to Natural calamities" },
+                    new MultiselectItem { Name = "Lack of Livelihood options" },
+                    new MultiselectItem { Name = "Civil and Political unrest" },
+                    new MultiselectItem { Name = "Border Interstate" },
+                    new MultiselectItem { Name = "Border International" }
+                };
+                return _localIssuesItems;
+            }
+        }
+    }
+}
--- NexGenRedAlert/NexGenRedAlert/Views/PreSvpFormPage.xaml.cs
+++ NexGenRedAlert/NexGenRedAlert/Views/PreSvpFormPage.xaml.cs
@@ -1,7 +1,7 @@
 ﻿using NexGenRedAlert.Models;
+using NexGenRedAlert.Services;
 using NexGenRedAlert.ViewModels;
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -15,6 +15,7 @@ namespace NexGenRedAlert.Views
         private List<string> PreviousAwarenessList =new List<string>();
         private MultipickerPage<MultiselectItem> multipickerSourceOfIncomePage;
         private MultipickerPage<MultiselectItem> multipickerLocalIssuesPage;
+        public MultipickerServices MultipickerServices = new MultipickerServices();
 
         public PreSvpFormPage()
 		{
@@ -34,34 +35,18 @@ namespace NexGenRedAlert.Views
 
         async void OnFocusedMajorEmploymentEntry(Object sender, EventArgs evt)
         {
-            var selectedItemList = new List<MultiselectItem>
-            {
-                new MultiselectItem { Name = "Agriculture" },
-                new MultiselectItem { Name = "Industrial" },
-                new MultiselectItem { Name = "Casual Labour" },
-                new MultiselectItem { Name = "Self-Employed" },
-                new MultiselectItem { Name = "Other" }
-            };
             if (multipickerSourceOfIncomePage == null)
             {
-                multipickerSourceOfIncomePage = new MultipickerPage<MultiselectItem>(selectedItemList) { Title = "Choose Applicable" };
+                multipickerSourceOfIncomePage = new MultipickerPage<MultiselectItem>(MultipickerServices.MajorSourceOfIncomeItems) { Title = "Choose Applicable" };
             }
             await Navigation.PushAsync(multipickerSourceOfIncomePage);
         }
 
         async void OnFocusedLocalIssuesEntry(object sender, EventArgs evt)
         {
-            var selectedItemList = new List<MultiselectItem>
-            {
-                new MultiselectItem { Name = "Prone to Natural calamities" },
-                new MultiselectItem { Name = "Lack of Livelihood options" },
-                new MultiselectItem { Name = "Civil and Political unrest" },
-                new MultiselectItem { Name = "Border Interstate" },
-                new MultiselectItem { Name = "Border International" }
-            };
             if (multipickerLocalIssuesPage == null)
             {
-                multipickerLocalIssuesPage = new MultipickerPage<MultiselectItem>(selectedItemList) { Title = "Choose Applicable" };
+                multipickerLocalIssuesPage = new MultipickerPage<MultiselectItem>(MultipickerServices.LocalIssuesItems) { Title = "Choose Applicable" };
             }
             await Navigation.PushAsync(multipickerLocalIssuesPage);
 
